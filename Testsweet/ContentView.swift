92870df0c04b6243dfba3b31b9338d3f0b9a17ea
Duy Nguyen <dnguyen19899@gmail.com>
2021-07-29 16:06:16 -0700

    Added Entries Range Generator and Timestamp in Milliseconds conversion.

--- Testsweet/ContentView.swift
+++ Testsweet/ContentView.swift
@@ -10,53 +10,111 @@ import SwiftUI
 struct ContentView: View {
 
     @State private var date = Date()
+    @State private var startDate = Date()
+    @State private var endDate = Date()
     
     var body: some View {
         
         VStack (alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/,
                 content: {
+                    // ----------- Custom Entry ----------- //
                     Form {
-                        DatePicker("Select a date and time", selection: $date, displayedComponents: [.date, .hourAndMinute])
+                        Text("Make Custom Entry").bold()
+                        DatePicker("Select date and time", selection: $date, displayedComponents: [.date, .hourAndMinute])
+                        
+                        let NSController = NightscoutController(date: date)
                         
                         HStack {
                             Spacer()
                             Button(action: {
                                 
                                 print("making post")
-                                let NSController = NightscoutController(date: date)
-                                //NSController.formatDateString()
                                 NSController.makeEntryPostRequest(dateString: "2021-07-29T20:59:27.499Z", date: 1627592367499 , sgv: 130, direction: "FLAT")
                             }){
-                                Text("Create Entry")
+                                Text("Create")
                                 .bold()
                                     .font(Font.custom("Helvetica Neue", size: 20.0))
-                                .padding(20)
+                                    .padding(.top, 15)
+                                    .padding(.bottom, 15)
+                                    .padding(.leading, 30)
+                                    .padding(.trailing, 30)
                                 .foregroundColor(Color.white)
                                 .background(Color.green)
                                 .cornerRadius(12)
                             }
-                            
                             Spacer()
-                            
+                        }
+                        HStack {
+                            Spacer()
+                            Button(action: {
+                                
+                                print("making delete")
+                                NSController.deleteEntryRequest()
+                            }){
+                                Text("Delete")
+                                .bold()
+                                    .font(Font.custom("Helvetica Neue", size: 15.0))
+                                    .padding(.top, 15)
+                                    .padding(.bottom, 15)
+                                    .padding(.leading, 30)
+                                    .padding(.trailing, 30)
+                                .foregroundColor(Color.white)
+                                .background(Color.red)
+                                .cornerRadius(12)
+                            }
+                            Spacer()
                         }
                     }
-                        Button(action: {
-                            
-                            print("making delete")
-                            let NSController = NightscoutController(date: date)
-                            //NSController.formatDateString()
-                            NSController.deleteEntryRequest()
-                        }){
-                            Text("Delete Entries")
-                            .bold()
-                                .font(Font.custom("Helvetica Neue", size: 20.0))
-                            .padding(20)
-                            .foregroundColor(Color.white)
-                            .background(Color.red)
-                            .cornerRadius(12)
-
+                    
+                    // ----------- Time Range Feature ----------- //
+                    Form {
+                        Text("Entries Generator").bold()
+                        DatePicker("Select a start date", selection: $startDate, displayedComponents: [.date, .hourAndMinute])
+                        DatePicker("Select an end date", selection: $endDate, displayedComponents: [.date, .hourAndMinute])
+                        
+                        let NSController = NightscoutController(startDate: startDate, endDate: endDate)
+                        
+                        HStack {
+                            Spacer()
+                            Button(action: {
+                                
+                                print("making post")
+                                // NSController.makeEntryPostRequest(dateString: "2021-07-29T20:59:27.499Z", date: 1627592367499 , sgv: 130, direction: "FLAT")
+                            }){
+                                Text("Create")
+                                .bold()
+                                    .font(Font.custom("Helvetica Neue", size: 20.0))
+                                    .padding(.top, 15)
+                                    .padding(.bottom, 15)
+                                    .padding(.leading, 30)
+                                    .padding(.trailing, 30)
+                                .foregroundColor(Color.white)
+                                .background(Color.green)
+                                .cornerRadius(12)
+                            }
+                            Spacer()
+                        }
+                        HStack {
+                            Spacer()
+                            Button(action: {
+                                
+                                print("making delete")
+                                // NSController.deleteEntryRequest()
+                            }){
+                                Text("Delete")
+                                .bold()
+                                    .font(Font.custom("Helvetica Neue", size: 15.0))
+                                    .padding(.top, 15)
+                                    .padding(.bottom, 15)
+                                    .padding(.leading, 30)
+                                    .padding(.trailing, 30)
+                                .foregroundColor(Color.white)
+                                .background(Color.red)
+                                .cornerRadius(12)
+                            }
+                            Spacer()
+                        }
                     }
-                        Spacer()
                 })
     }
 }
--- Testsweet/NightscoutController.swift
+++ Testsweet/NightscoutController.swift
@@ -7,25 +7,69 @@
 
 import Foundation
 
+extension Date {
+    var milliStamp:Int64 {
+        return Int64((self.timeIntervalSince1970 * 1000.0).rounded())
+    }
+}
+
 class NightscoutController {
     
-    private var date: Date
-    private var timestamp = 0
+    private var debug = true
+    private var date: Date = Date()
+    private var timeStamp:Int64 = 0
     private var dateString = ""
+    
+    private var startDate: Date = Date()
+    private var startTimeStamp:Int64 = 0
+    private var startDateString = ""
+    
+    private var endDate: Date = Date()
+    private var endTimeStamp:Int64 = 0
+    private var endDateString = ""
+    
     
     init (date: Date) {
         self.date = date
-        // print(self.date)
+        self.timeStamp = self.date.milliStamp
+        self.dateString = self.formatDateString(date: self.date)
+        
+        if debug {
+            print("DATE: \(self.date)")
+            print("TIMESTAMP IN MILLISECONDS: \(self.timeStamp)")
+            print("DATE STRING: \(self.dateString)")
+            print(" ")
+        }
     }
     
-    func formatDateString() {
+    init (startDate: Date, endDate: Date)
+    {
+        self.startDate = startDate
+        self.startTimeStamp = self.startDate.milliStamp
+        self.startDateString = self.formatDateString(date: self.startDate)
+        if debug {
+            print("START DATE: \(self.startDate)")
+            print("START TIMESTAMP IN MILLISECONDS: \(self.startTimeStamp)")
+            print("START DATE STRING: \(self.startDateString)")
+            print(" ")
+        }
+        
+        self.endDate = endDate
+        self.endTimeStamp = self.endDate.milliStamp
+        self.endDateString = self.formatDateString(date: self.endDate)
+        if debug {
+            print("END DATE: \(self.endDate)")
+            print("END TIMESTAMP IN MILLISECONDS: \(self.endTimeStamp)")
+            print("END DATE STRING: \(self.endDateString)")
+            print(" ")
+        }
+    }
+    
+    func formatDateString(date: Date) -> String {
         
         let iso8601DateFormatter = ISO8601DateFormatter()
         iso8601DateFormatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
-        self.dateString = iso8601DateFormatter.string(from: self.date)
-        
-        print(self.date)
-        print(self.dateString)
+        return iso8601DateFormatter.string(from: self.date)
     }
 
     // This posts the entrys that we input as parameters.
