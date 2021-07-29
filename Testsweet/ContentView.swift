59c5911c34cf2c6ceb0916219842bf8ca758b1af
Jayden Morgan <jaydenhmorgan@gmail.com>
2021-07-29 14:56:40 -0600

    Added delete and delete button. Deleted treatments also.

--- Testsweet/ContentView.swift
+++ Testsweet/ContentView.swift
@@ -22,10 +22,10 @@ struct ContentView: View {
                             Spacer()
                             Button(action: {
                                 
-                                print("making request button pressed")
+                                print("making post")
                                 let NSController = NightscoutController(date: date)
-                                NSController.formatDateString()
-//                                NSController.makeEntryPostRequest(dateString: "2021-07-27T22:28:57.352Z", date: 1627424937 , sgv: 170, direction: "FLAT")
+                                //NSController.formatDateString()
+                                NSController.makeEntryPostRequest(dateString: "2021-07-28T20:57:49.051Z", date: 1627505869051 , sgv: 130, direction: "FLAT")
                             }){
                                 Text("Create Entry")
                                 .bold()
@@ -35,10 +35,29 @@ struct ContentView: View {
                                 .background(Color.green)
                                 .cornerRadius(12)
                             }
+                            
                             Spacer()
+                            
                         }
                     }
-        })
+                        Button(action: {
+                            
+                            print("making delete")
+                            let NSController = NightscoutController(date: date)
+                            //NSController.formatDateString()
+                            NSController.deleteEntryRequest()
+                        }){
+                            Text("Delete Entries")
+                            .bold()
+                                .font(Font.custom("Helvetica Neue", size: 20.0))
+                            .padding(20)
+                            .foregroundColor(Color.white)
+                            .background(Color.red)
+                            .cornerRadius(12)
+
+                    }
+                        Spacer()
+                })
     }
 }
 
--- Testsweet/NightscoutController.swift
+++ Testsweet/NightscoutController.swift
@@ -27,45 +27,6 @@ class NightscoutController {
         print(self.date)
         print(self.dateString)
     }
-    
-    // Make treatment and post it on the nighscout site, I can delete this but I am not ready to part with it until we know for sure.
-    func makeTreatmentPostRequest(id: String, glucose: String, carbs: Double, protein: Double, fat: Double, insulin: Double, enteredBy: String ) {
-
-        guard let url = URL(string: "https://test-sweet.herokuapp.com/api/v1/treatments?token=api-d1b60b0ce9c2dbae"),
-
-            let payload = "[{\"_id\":\"\(id)\",\"eventType\":\"testing\",\"created_at\":\"string\",\"glucose\":\"\(glucose)\",\"glucoseType\":\"string\",\"carbs\":\(carbs),\"protein\":\(protein),\"fat\":\(fat),\"insulin\":\(insulin),\"units\":\"string\",\"transmitterId\":\"string\",\"sensorCode\":\"string\",\"notes\":\"Testing on mac\",\"enteredBy\":\"\(enteredBy)\"}]".data(using: .utf8) else
-
-        {
-            return
-        }
-
-        var request = URLRequest(url: url)
-
-        request.httpMethod = "POST"
-
-        request.addValue("TandemDiabetes1", forHTTPHeaderField: "x-api-key")
-
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-
-        request.httpBody = payload
-
-
-     
-
-        URLSession.shared.dataTask(with: request) { (data, response, error) in
-
-            guard error == nil else { print(error!.localizedDescription); return }
-
-            guard let data = data else { print("Empty data"); return }
-
-            if let str = String(data: data, encoding: .utf8) {
-
-                print(str)
-
-            }
-        }.resume()
-    }
-    
 
     // This posts the entrys that we input as parameters.
     func makeEntryPostRequest(dateString: String, date: Int, sgv: Int, direction: String) {
@@ -84,29 +45,40 @@ class NightscoutController {
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpBody = payload
 
-
-     
-
         URLSession.shared.dataTask(with: request) { (data, response, error) in
-
             guard error == nil else { print(error!.localizedDescription); return }
-
             guard let data = data else { print("Empty data"); return }
-
             if let str = String(data: data, encoding: .utf8) {
-
                 print(str)
 
             }
 
         }.resume()
     }
+    
+    //deleteEntryRequest function deletes all entries
+    func deleteEntryRequest() {
+        print("deleting")
+        guard let url = URL(string: "https://test-sweet.herokuapp.com/api/v1/entries.json?find\\[sgv\\]=130&token=api-d1b60b0ce9c2dbae")
 
-    func thisIsATest(){
-        print("this is a test.")
-    }
-    func duyPrint() {
-        print("testing branches")
+        else {
+            print("else\\")
+            return
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "DELETE"
+        request.addValue("TandemDiabetes1", forHTTPHeaderField: "x-api-key")
+
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard error == nil else { print(error!.localizedDescription); return }
+            guard let data = data else { print("Empty data"); return }
+            if let str = String(data: data, encoding: .utf8) {
+                print(str)
+            }
+
+        }.resume()
+        
     }
 }
 
